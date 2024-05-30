466fe89c35ebfc97ca784334e88c85c8d350f347
Kairi Purnell <kp2845@nyu.edu>
2024-05-29 22:58:23 -0400

    spacing updates to bmi files

--- bmiimperial.py
+++ bmiimperial.py
@@ -1,9 +1,9 @@
-impweight = float (input("Enter your weight in pounds: "))
-impheight = float (input("Enter your height in inches: "))
+impweight = float(input("Enter your weight in pounds: "))
+impheight = float(input("Enter your height in inches: "))
 weight = impweight * 0.453592
-#print("Here is your weight in KG: " + str(weight))
+# print("Here is your weight in KG: " + str(weight))
 height = impheight * 0.0254
-#print("Here is your height in meters: " + str(height))
+# print("Here is your height in meters: " + str(height))
 BMI = weight/(height*height)
 print("BMI is {:.10f}".format(BMI))
-#print("BMI is " + str(BMI))
+# print("BMI is " + str(BMI))
